be0fd4def4b564aed65beb339519d6633c18a1eb
busayo <prosperotemuyiwa@gmail.com>
2015-06-22 15:01:55 +0100

    enable trust proxy

--- server.js
+++ server.js
@@ -28,33 +28,21 @@ var app = express();
 /**
  * Express configuration.
  */
-// if(process.env.NODE_ENV == "production"){
-//   app.use (function (req, res, next) {
-//     if (req.secure) {
-//       // request was via https, so do no special handling
-//       next();
-//     } else {
-//       // request was via http, so redirect to https
-//       res.redirect('https://' + req.headers.host + req.url);
-//     }
-// });
-// }
+app.enable('trust proxy');
 app.use (function (req, res, next) {
-    if (req.secure) {
-      // request was via https, so do no special handling
+    if(req.secure) {
+      //request was via https, so do no special handling
       next();
     } else {
-      // request was via http, so redirect to https
+      //request was via http, so redirect to https
       res.redirect('https://' + req.headers.host + req.url);
     }
 });
-
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true})); //use bodyParser for request and parsing info
 app.use(bodyParser.json());
 app.use(express.static( __dirname + '/public')); //use to serve static files like favicon, css, angular and the rest
-
 
 /**
  * Routes Configuration
