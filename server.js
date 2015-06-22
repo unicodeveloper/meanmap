9bed323d3bab2063585327c5468254e5bcc0fc1d
busayo <prosperotemuyiwa@gmail.com>
2015-06-22 15:12:13 +0100

    check environment before forcing ssl

--- server.js
+++ server.js
@@ -28,16 +28,19 @@ var app = express();
 /**
  * Express configuration.
  */
-app.enable('trust proxy');
-app.use (function (req, res, next) {
-    if(req.secure) {
-      //request was via https, so do no special handling
-      next();
-    } else {
-      //request was via http, so redirect to https
-      res.redirect('https://' + req.headers.host + req.url);
-    }
-});
+if(process.env.NODE_ENV == 'production'){
+  app.enable('trust proxy');
+  app.use (function (req, res, next) {
+      if(req.secure) {
+        //request was via https, so do no special handling
+        next();
+      } else {
+        //request was via http, so redirect to https
+        res.redirect('https://' + req.headers.host + req.url);
+      }
+  });
+}
+
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true})); //use bodyParser for request and parsing info
