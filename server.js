c1caa1e530a84c1d6f288e85cd1b54271e9e7e5e
busayo <prosperotemuyiwa@gmail.com>
2015-06-22 14:48:21 +0100

    Put the https redirect back

--- server.js
+++ server.js
@@ -39,6 +39,15 @@ var app = express();
 //     }
 // });
 // }
+app.use (function (req, res, next) {
+    if (req.secure) {
+      // request was via https, so do no special handling
+      next();
+    } else {
+      // request was via http, so redirect to https
+      res.redirect('https://' + req.headers.host + req.url);
+    }
+});
 
 app.use(cors());
 app.use(morgan('dev'));
