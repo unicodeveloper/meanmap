1082eb37c0fae5e0a8e78722e338a48803dabbe8
busayo <prosperotemuyiwa@gmail.com>
2015-07-20 11:40:32 +0100

    Add Self-Explanatory Comments

--- server/controllers/newsletter.server.controller.js
+++ server/controllers/newsletter.server.controller.js
@@ -5,6 +5,13 @@ var nodemailer    = require('nodemailer'),
     MC            = new MCapi.Mailchimp(secrets.mailChimp.apiKey);
 
 module.exports = {
+  /**
+   *  Subscribe A User to Meanmap Newsletter
+   * @param  {void}   req
+   * @param  {void}   res
+   * @param  {Function} next
+   * @return {object}
+   */
   subscribe: function(req, res, next){
     var email  = req.body.email;
     var listID = secrets.mailChimp.listID;
@@ -17,12 +24,11 @@ module.exports = {
         EMAIL: email
       }
     };
-
     // submit subscription request to mail-chimp
     MC.lists.subscribe(mcReq, function(data) {
-        console.log(data);
-        res.json({ success:true, message: data });
-        next();
+      console.log(data);
+      res.json({ success:true, message: data });
+      next();
     }, function(error) {
         console.log(error);
         res.json({ success:false, error: error });
